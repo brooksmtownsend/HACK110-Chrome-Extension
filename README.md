c9f461d1e9ea5824494f3f0c05739ed042f7adcb
Brooks Townsend <brooksmtownsend@gmail.com>
2018-03-23 10:38:38 -0400

    Updates for HACK110

--- README.md
+++ README.md
@@ -2,3 +2,12 @@
 Here are the setup files for your chrome extension. Follow these instructions to make your own!
 
 # Instructions
+1. Download the setup files by going into VSCode and:
+  1. Click View -> Command Palette
+  2. Type `git clone` -> Hit Enter
+  3. Paste `https://github.com/brooksmtownsend/HACK110-Chrome-Extension.git` as the repository URL
+  4. Hit enter twice to clone it to your home directory
+  5. Open the newly cloned repository
+2. Modify `manifest.json` to reflect your Chrome extensions purpose.
+3. Modify `content.js` to replace text of your choice.
+4. Open up Google Chrome and navigate to `chrome://extensions` like you would a website
--- content.js
+++ content.js
@@ -1,15 +1,37 @@
 var elements = document.getElementsByTagName('*');
 
+// Loop through every element in the DOM (Document Object Model)
 for (var i = 0; i < elements.length; i++) {
     var element = elements[i];
 
+    // Loop through every elements children
     for (var j = 0; j < element.childNodes.length; j++) {
         var node = element.childNodes[j];
 
+        //If the element we find is a text element
         if (node.nodeType === 3) {
             var text = node.nodeValue;
-            var replacedText = text.replace(/[word or phrase to replace here]/gi, '[new word or phrase]');
 
+            // Here's the magic, we see replace one string of text with another string of text.
+            var replacedText = text.replace(/COMP110|COMP 110/gi, "The best class at UNC");
+
+            // Uncomment the lines of code below for some more examples of text replacement
+
+            // You can also do this with function literals!
+            // var replacedText = text.replace(/COMP110|COMP 110/gi, (s) => "The best class at UNC");
+
+            // What if we wanted to make sure COMP was always uppercase?
+            // var replacedText = text.replace(/comp/gi, (s) => s.toUpperCase());
+          
+            // What if we wanted to reverse ALL strings?
+            // var replacedText = text.replace(/\w+/gi, (s) => {
+            //    let reversedString = "";
+            //    for (let i = s.length - 1; i >= 0; i--) {
+            //        newString += s[i];
+            //    }
+            //    return reversedString;
+            // });
+          
             if (replacedText !== text) {
                 element.replaceChild(document.createTextNode(replacedText), node);
             }
